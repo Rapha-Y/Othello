a6a20d3f96b45bf997492708590bf7577bfdae36
Rapha-Y <raph.yuki@gmail.com>
2020-06-15 15:31:05 -0300

    added board coordinates

--- Othello/Othello/Board.cs
+++ Othello/Othello/Board.cs
@@ -6,43 +6,64 @@ namespace Othello
 {
     class Board
     {
-        private int[,] GameBoard = new int[8, 8];
+        //8x8 board w/room for coordinates
+        private char[,] GameBoard = new char[9, 9];
         public Board()
         {
             this.InitBoard();
         }
         public void InitBoard()
         {
-            for (int i = 0; i < 8; i++)
+            //clear board
+            for (int i = 0; i < 9; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < 9; j++)
                 {
-                    this.GameBoard[i, j] = 0;
+                    this.GameBoard[i, j] = ' ';
                 }
             }
-            this.GameBoard[3, 3] = -1;
-            this.GameBoard[3, 4] = 1;
-            this.GameBoard[4, 3] = 1;
-            this.GameBoard[4, 4] = -1;
+
+            //letter labels for 1st column
+            for (int i = 1; i < 9; i++)
+            {
+                this.GameBoard[i, 0] = (char) (i+64);
+            }
+
+            //number labels for 1st row
+            for (int j = 1; j < 9; j++)
+            {
+                this.GameBoard[0, j] = (char) (j+48);
+            }
+
+            //central stones
+            this.GameBoard[4, 4] = '\u25CF';
+            this.GameBoard[4, 5] = '\u25CB';
+            this.GameBoard[5, 4] = '\u25CB';
+            this.GameBoard[5, 5] = '\u25CF';
         }
         public void ShowBoard()
         {
-            for(int i = 0; i < 8; i ++)
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            int i = 0;
+            int j = 0;
+
+            //empty space
+            Console.Out.Write("   ");
+
+            //1st row, only row with a different 1st column
+            for (j = 1; j < 9; j++)
             {
-                for(int j = 0; j < 8; j++)
+                Console.Out.Write($"[{this.GameBoard[i, j]}]");
+            }
+            Console.Out.WriteLine();
+
+            //2nd to 9th rows
+            for (i = 1; i < 9; i++)
+            {
+                for (j = 0; j < 9; j++)
                 {
-                    switch(this.GameBoard[i, j])
-                    {
-                        case -1:
-                            Console.Out.Write("[W]");
-                            break;
-                        case 0:
-                            Console.Out.Write("[E]");
-                            break;
-                        case 1:
-                            Console.Out.Write("[B]");
-                            break;
-                    }
+                    Console.Out.Write($"[{this.GameBoard[i, j]}]");
                 }
                 Console.Out.WriteLine();
             }
